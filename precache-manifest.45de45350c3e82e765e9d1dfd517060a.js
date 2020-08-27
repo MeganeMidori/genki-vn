ef90107f89b753038a115244aaeea39e263116da
MeganeMidori <MeganeMidori@users.noreply.github.com>
2020-08-27 13:47:59 -0700

    Updates

---
+++ precache-manifest.45de45350c3e82e765e9d1dfd517060a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93332233cece647e75d7a8d13641319a",
+    "url": "/genki-vn/index.html"
+  },
+  {
+    "revision": "54be5f1a415996e82f7f",
+    "url": "/genki-vn/static/css/2.01a96852.chunk.css"
+  },
+  {
+    "revision": "f573c02f844817af8c89",
+    "url": "/genki-vn/static/css/main.a4d56969.chunk.css"
+  },
+  {
+    "revision": "54be5f1a415996e82f7f",
+    "url": "/genki-vn/static/js/2.adf54f95.chunk.js"
+  },
+  {
+    "revision": "9958780d182350cee9cc1b612e70eb1e",
+    "url": "/genki-vn/static/js/2.adf54f95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f573c02f844817af8c89",
+    "url": "/genki-vn/static/js/main.9204961d.chunk.js"
+  },
+  {
+    "revision": "e1da1cb27ac37ceb4128",
+    "url": "/genki-vn/static/js/runtime-main.d2721a36.js"
+  }
+]);
